90e011e39f63e4cc1438bd28f7d26b667b18e13c
Prashant <mapraashant@gmail.com>
2026-05-02 23:12:39 +1000

    Fix App.jsx lint warning s

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
-import { useState,useEffect } from 'react';
+import { useState } from 'react';
 import ChatWidget from './components/ChatWidget';
 import { ChatFooter } from './components/ChatFooter';
 import Salon from './components/Salon';
 import './App.css'
 
 function App() {
-  const [businessId, setBusinessId] = useState(() => {
+//   Lazy Initialization.Initial Calculation: When the component loads, React sees the () => { ... } function inside useState. It pauses, runs that function, reads the URL, and finds the ID ("123").
+// Render 1: React immediately sets the initial state to "123". It renders <Salon businessId={"123"} /> and <ChatWidget businessId={"123"} />.
+// Mount: The components are placed on the screen.
+// Done!
+
+  const [businessId] = useState(() => {
     const params = new URLSearchParams(window.location.search);
     const id = params.get('businessId');
     console.log("Business ID: ", id);
